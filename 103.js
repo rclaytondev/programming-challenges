c9bbe47834a38438917d005e11ec6204b8af7d2c
HappyRyan2 <happyryan2@hotmail.com>
2021-04-03 12:09:06 -0700

    refactored solution to problem 103

--- 103.js
+++ 103.js
@@ -30,53 +30,33 @@ const problem103 = {
 			new Set(array).intersection(new Set(numbersToSkip)).size === 0
 		);
 	},
-	specialSet: (size, specialSumArray = [], upperBound = Infinity, numbersToSkip = []) => {
-		/*
-		Returns a special set (in array form, in ascending order) with the given size, starting with the contents of specialSumArray.
-		All numbers will be lower than `upperBound`, and will not include anything in `numbersToSkip`.
-		`specialSumArray`, `upperBound`, and `numbersToSkip` are mostly just used internally.
-		*/
-		if(specialSumArray.length === size) {
-			if(problem103.isValidArray(specialSumArray, upperBound, numbersToSkip)) {
-				return specialSumArray;
-			}
-			return null;
+	specialSet: (size, specialSumArray = []) => {
+		if(size === specialSumArray.length) {
+			return specialSumArray;
 		}
-		let lowestSetArray = null;
+		const { numbersToSkip, upperBound } = problem103.invalidNumbers(specialSumArray);
+		let lowestSumArray = null;
 		let lowestSum = Infinity;
+		/*
+		size == 5
+		specialSumArray == [4]
+		No solutions will be found (see proof in 103.md)
+		The upper bound is infinity (since there's only 1 item in specialSumArray)
+		It will loop forever.
+		*/
 		for(
-			let nextNumber = specialSumArray.length ? specialSumArray.max() + 1 : Math.max(size - 1, 1);
-			nextNumber < upperBound && nextNumber < lowestSum;
+			let nextNumber = specialSumArray.length ? specialSumArray.max() + 1 : Math.max(1, size - 1);
+			nextNumber < Math.min(upperBound, lowestSum);
 			nextNumber ++
 		) {
-			if(numbersToSkip.includes(nextNumber)) {
-				continue;
-			}
-
-			const newSpecialSumArray = [...specialSumArray, nextNumber];
-			const {
-				numbersToSkip: newNumbersToSkip,
-				upperBound: newUpperBound
-			} = problem103.invalidNumbers(newSpecialSumArray);
-			if(size - newSpecialSumArray.length > newUpperBound - newSpecialSumArray.max() - 1) {
-				continue;
-			}
-
-			const result = problem103.specialSet(
-				size,
-				newSpecialSumArray,
-				newUpperBound,
-				newNumbersToSkip
-			);
-			if(result != null) {
-				const resultSum = [...result].sum();
-				if(resultSum < lowestSum) {
-					lowestSum = resultSum;
-					lowestSetArray = result;
-				}
+			if(numbersToSkip.includes(nextNumber)) { continue; }
+			const result = problem103.specialSet(size, [...specialSumArray, nextNumber]);
+			if(result != null && result.sum() < lowestSum) {
+				lowestSumArray = result;
+				lowestSum = result.sum();
 			}
 		}
-		return lowestSetArray;
+		return lowestSumArray;
 	},
 };
 
@@ -85,8 +65,8 @@ testing.addUnit("problem103.specialSet()", problem103.specialSet, [
 	[2, [1, 2]],
 	[3, [2, 3, 4]],
 	[4, [3, 5, 6, 7]],
-	// [5, [6, 9, 11, 12, 13]],
-	// [6, [11, 18, 19, 20, 22, 25]]
+	[5, [6, 9, 11, 12, 13]],
+	[6, [11, 18, 19, 20, 22, 25]]
 ]);
 // testing.testUnit("problem103.specialSet()");
 testing.runTestByName("problem103.specialSet() - test case 4");
