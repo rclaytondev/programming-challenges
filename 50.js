af4d6fbb15c9f8c6bc90aa38db32d79b4fc1d992
HappyRyan2 <happyryan2@hotmail.com>
2021-04-17 17:22:42 -0700

    added solution to problem 50

--- 50.js
+++ 50.js
@@ -1,18 +1,30 @@
-const solve = () => {
+const solve = (upperBound = 1e6) => {
 	let primesBelow = [];
 	let highestPrime = -Infinity;
-	for(const prime of Sequence.PRIMES) {
-		for(const subArray of primesBelow.subArrays()) {
-			if(subArray.sum() === prime) {
-				highestPrime = prime;
-				break;
+	let mostPrimes = -Infinity;
+	loop1: for(const prime of Sequence.PRIMES) {
+		if(prime >= upperBound) {
+			break;
+		}
+
+		loop2: for(let start = 0; start < primesBelow.length; start ++) {
+			let sum = 0;
+			loop3: for(let end = start; end < primesBelow.length; end ++) {
+				sum += primesBelow[end];
+				if(sum > prime) { break loop3; }
+				else if(sum === prime && end - start > mostPrimes) {
+					mostPrimes = end - start;
+					highestPrime = prime;
+					break loop2;
+				}
 			}
 		}
 
 		primesBelow.push(prime);
-		if(prime >= 1e6) {
-			break;
-		}
 	}
 	return highestPrime;
 };
+
+console.time("solving the problem");
+console.log(solve());
+console.timeEnd("solving the problem");
