4fc10e018cc8506db78e67fe1b87469f5475aad5
HappyRyan2 <happyryan2@hotmail.com>
2021-05-18 20:52:13 -0700

    added method fractalSequenceSum

--- 535.js
+++ 535.js
@@ -27,3 +27,23 @@ testing.addUnit("fractalSequence", {
 		expect(terms).toEqual([1, 1, 2, 1, 3, 2, 4, 1, 5, 3]);
 	}
 });
+
+const fractalSequenceSum = (numTerms) => {
+	const terms = fractalSequence.slice(0, numTerms);
+	return terms.sum();
+};
+
+testing.addUnit("fractalSequenceSum()", {
+	"returns the correct result for 1 term": () => {
+		const sum = fractalSequenceSum(1);
+		expect(sum).toEqual(1);
+	},
+	"returns the correct result for 20 terms": () => {
+		const sum = fractalSequenceSum(20);
+		expect(sum).toEqual(86);
+	},
+	"returns the correct result for 1000 terms": () => {
+		const sum = fractalSequenceSum(1000);
+		expect(sum).toEqual(364089);
+	}
+});
