b8b63db497c4948addc92b1a53ade74d920f419d
HappyRyan2 <happyryan2@hotmail.com>
2021-05-19 10:30:08 -0700

    added method sequentialTermsBelow

--- 535.js
+++ 535.js
@@ -47,3 +47,40 @@ testing.addUnit("fractalSequenceSum()", {
 		expect(sum).toEqual(364089);
 	}
 });
+
+const sequentialTermsBelow = (numTerms) => {
+	/* Returns the number of sequential terms whose one-based
+	indices are less than or equal to the given number of terms. */
+	let totalTerms = 0;
+	let sequentialTerms = 0;
+	for(const term of fractalSequence) {
+		const sqrt = flooredSqrt(term);
+		if(totalTerms + sqrt >= numTerms) {
+			return sequentialTerms + (numTerms - totalTerms);
+		}
+		totalTerms += sqrt + 1;
+		sequentialTerms += sqrt;
+	}
+};
+testing.addUnit("sequentialTermsBelow()", sequentialTermsBelow, [
+	[1, 1],
+	[2, 1],
+	[3, 2],
+	[4, 2],
+	[5, 3],
+	[6, 3],
+	[7, 4],
+	[8, 4],
+	[9, 5],
+	[10, 5],
+	[11, 6],
+	[12, 6],
+	[13, 7],
+	[14, 8],
+	[15, 8],
+	[16, 9],
+	[17, 9],
+	[18, 10],
+	[19, 11],
+	[20, 11]
+]);
