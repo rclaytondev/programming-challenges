e78a5a968c8f37ff2daaa5390ff7b1597c97129d
HappyRyan2 <happyryan2@hotmail.com>
2021-05-19 10:40:52 -0700

    changed all methods to use BigInts

--- 535.js
+++ 535.js
@@ -31,62 +31,63 @@ testing.addUnit("fractalSequence", {
 const fractalSequenceSum = (numTerms) => {
 	if(numTerms <= 2) { return numTerms; }
 
+	numTerms = BigInt(numTerms);
 	const sequentialTerms = sequentialTermsBelow(numTerms);
 	const fractalTerms = numTerms - sequentialTerms;
 
-	const sumOfSequentials = (sequentialTerms) * (sequentialTerms + 1) / 2;
+	const sumOfSequentials = (sequentialTerms) * (sequentialTerms + 1n) / 2n;
 	const sumOfFractals = fractalSequenceSum(fractalTerms);
 	return sumOfSequentials + sumOfFractals;
 };
 
 testing.addUnit("fractalSequenceSum()", {
 	"returns the correct result for 1 term": () => {
-		const sum = fractalSequenceSum(1);
-		expect(sum).toEqual(1);
+		const sum = fractalSequenceSum(1n);
+		expect(sum).toEqual(1n);
 	},
 	"returns the correct result for 20 terms": () => {
-		const sum = fractalSequenceSum(20);
-		expect(sum).toEqual(86);
+		const sum = fractalSequenceSum(20n);
+		expect(sum).toEqual(86n);
 	},
 	"returns the correct result for 1000 terms": () => {
-		const sum = fractalSequenceSum(1000);
-		expect(sum).toEqual(364089);
+		const sum = fractalSequenceSum(1000n);
+		expect(sum).toEqual(364089n);
 	}
 });
 
 const sequentialTermsBelow = (numTerms) => {
 	/* Returns the number of sequential terms whose one-based
 	indices are less than or equal to the given number of terms. */
-	let totalTerms = 0;
-	let sequentialTerms = 0;
+	let totalTerms = 0n;
+	let sequentialTerms = 0n;
 	for(const term of fractalSequence) {
-		const sqrt = flooredSqrt(term);
+		const sqrt = BigInt(flooredSqrt(term));
 		if(totalTerms + sqrt >= numTerms) {
 			return sequentialTerms + (numTerms - totalTerms);
 		}
-		totalTerms += sqrt + 1;
+		totalTerms += sqrt + 1n;
 		sequentialTerms += sqrt;
 	}
 };
 testing.addUnit("sequentialTermsBelow()", sequentialTermsBelow, [
-	[1, 1],
-	[2, 1],
-	[3, 2],
-	[4, 2],
-	[5, 3],
-	[6, 3],
-	[7, 4],
-	[8, 4],
-	[9, 5],
-	[10, 5],
-	[11, 6],
-	[12, 6],
-	[13, 7],
-	[14, 8],
-	[15, 8],
-	[16, 9],
-	[17, 9],
-	[18, 10],
-	[19, 11],
-	[20, 11]
+	[1n, 1n],
+	[2n, 1n],
+	[3n, 2n],
+	[4n, 2n],
+	[5n, 3n],
+	[6n, 3n],
+	[7n, 4n],
+	[8n, 4n],
+	[9n, 5n],
+	[10n, 5n],
+	[11n, 6n],
+	[12n, 6n],
+	[13n, 7n],
+	[14n, 8n],
+	[15n, 8n],
+	[16n, 9n],
+	[17n, 9n],
+	[18n, 10n],
+	[19n, 11n],
+	[20n, 11n]
 ]);
