f9cb98edb23ace032885adb609c3dec0e486f149
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-09-21 17:06:16 -0700

    added fast solution to problem 114

--- 114.js
+++ 114.js
@@ -1,43 +1,45 @@
-const numCombinations = (size) => {
-	const isValid = (combination) => {
-		if(combination.length > size) { return false; }
-		for(const [i, value] of combination.entries()) {
-			if(
-				value === "T" &&
-				(combination[i + 1] === "F" || (i === combination.length - 1 && combination.length === size)) &&
-				(
-					(combination[i - 1] ?? "F") === "F" || (combination[i - 2] ?? "F") === "F"
-				)
-			) { return false; }
-		}
-		return true;
-	};
-	let numWays = 0;
-	for(const combination of Tree.iterate([], (combination) =>
-		[
-			[...combination, "T"],
-			[...combination, "F"]
-		].filter(v => isValid(v))
-	)) {
-		if(isValid(combination) && combination.length === size) {
-			numWays ++;
+const getCombinations = (size) => {
+	if(size < 0) { return []; }
+	if(size < 3) { return [ new Array(size).fill(false) ]; }
+	if(size === 3) {
+		return [
+			[false, false, false],
+			[true, true, true]
+		];
+	}
+	let combinations = [];
+	combinations.push(new Array(size).fill(true));
+	for(let blockSize = 3; blockSize < size; blockSize ++) {
+		for(const combination of getCombinations(size - (blockSize + 1))) {
+			combinations.push([
+				...new Array(blockSize).fill(true),
+				false,
+				...combination
+			]);
 		}
 	}
-	return numWays;
+	for(const combination of getCombinations(size - 1)) {
+		combinations.push([ false, ...combination ]);
+	}
+	return combinations;
 };
-
-// const numCombinations = (size) => {
-// 	if(size < 3) { return 1; }
-// 	if(size === 3) { return 2; }
-// 	let numWays = 1;
-// 	for(let blockSize = 3; blockSize <= size; blockSize ++) {
-// 		numWays += numCombinations(size - (blockSize + 1));
-// 	}
-// 	for(let gapSize = 1; gapSize <= size; gapSize ++) {
-// 		numWays += numCombinations(size - gapSize);
-// 	}
-// 	return numWays;
-// };
+const numCombinations = ((size) => {
+	if(size < 0) { return 0; }
+	if(size < 3) { return 1; }
+	if(size === 3) { return 2; }
+	let numWays = 1;
+	for(let blockSize = 3; blockSize < size; blockSize ++) {
+		numWays += numCombinations(size - (blockSize + 1));
+	}
+	numWays += numCombinations(size - 1);
+	return numWays;
+}).memoize(true);
+testing.addUnit("getCombinations()", {
+	"returns the correct result for 4": () => {
+		const result = getCombinations(4);
+		expect(result.length).toEqual(4);
+	}
+});
 testing.addUnit("numCombinations()", {
 	"returns the correct result for 4": () => {
 		const result = numCombinations(4);
