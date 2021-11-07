8e18a7f5c46ac499fd86a32d6603cf496df9d703
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-07 14:43:17 -0800

    solution to problem 770 now uses precise rational-number calculations

--- 770.js
+++ 770.js
@@ -1,14 +1,14 @@
 const winnings = (function(givesLeft, takesLeft) {
-	if(givesLeft === 0) { return 1; }
-	if(takesLeft === 0) { return 2 ** givesLeft; }
+	if(givesLeft === 0) { return new Factorization([]); }
+	if(takesLeft === 0) { return new Factorization([givesLeft]); }
 	const multiplier1 = winnings(givesLeft, takesLeft - 1);
 	const multiplier2 = winnings(givesLeft - 1, takesLeft);
-	const optimalBet = (multiplier1 - multiplier2) / (multiplier1 + multiplier2);
-	const multiplier = multiplier1 * (1 - optimalBet);
+	const optimalBet = (multiplier1.subtract(multiplier2)).divide(multiplier1.add(multiplier2));
+	const multiplier = multiplier1.multiply(new Factorization(1).subtract(optimalBet));
 	return multiplier;
 }).memoize(true);
 testing.addUnit("winnings()", winnings, [
-	[1, 1, 4/3]
+	[1, 1, new Factorization([2, -1])]
 ]);
 
 const invWinnings = (desiredGold) => {
