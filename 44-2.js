1f531e70f5b0ad1f08b8c7be9b87231bd1f1720c
HappyRyan2 <HappyRyan2@Hotmail.com>
2021-11-10 20:30:38 -0800

    added a slightly faster algorithm for problem 44

--- 44-2.js
+++ 44-2.js
@@ -6,10 +6,21 @@ pentagonals.indexOf = (n) => {
 	const result = (1/2 + Math.sqrt(1/4 + 6 * n)) / 3 - 1;
 	return (result === Math.round(result)) ? result : -1;
 };
-
+pentagonals.includes = (n) => pentagonals.indexOf(n) !== -1;
 
 const solve = () => {
-	for(const [difference, index1] of pentagonals.entries()) {
-		
+	for(const [difference, j] of pentagonals.entries()) {
+		for(const [smaller, i] of pentagonals.entries()) {
+			const larger = smaller + difference;
+			const sum = smaller + larger;
+			if(pentagonals.includes(larger) && pentagonals.includes(sum)) {
+				console.log(`the answer is ${difference}`);
+				return difference;
+			}
+			if(3 * i + 4 > difference) { break; }
+		}
+		if(j % 10 === 0) {
+			console.log(difference);
+		}
 	}
 };
