dfdd74803d049e581a33f983e3fd8e62d91af05c
HappyRyan2 <HappyRyan2@Hotmail.com>
2022-02-19 19:34:18 -0800

    fixed bugs

--- 782-diagonal-symmetry.js
+++ 782-diagonal-symmetry.js
@@ -1,10 +1,10 @@
 const testConjecture = (size) => {
-	for(let numOnes = 0; numOnes <= size / 2; numOnes ++) {
+	for(let numOnes = 0; numOnes <= (size ** 2) / 2; numOnes ++) {
 		for(const grid of binaryGrids(size, numOnes)) {
 			const rows = grid.rows;
-			const columns = grid.columns;
-			const numDistinctRows = distinctRows(grid);
-			const numDistinctColumns = distinctColumns(grid);
+			const columns = grid.columns();
+			const numDistinctRows = distinctRows(grid).length;
+			const numDistinctColumns = distinctColumns(grid).length;
 			const complexity = findComplexity(grid);
 			if(numDistinctRows === 3 && numDistinctColumns === 3 && complexity === 3 && !rows.equals(columns)) {
 				console.log(`counterexample:`);
