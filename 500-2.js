d7fdd7cc4e85a21070289a49bc6d416759e2a486
HappyRyan2 <HappyRyan2@Hotmail.com>
2022-03-01 20:33:33 -0800

    added slow-but-correct algorithm for problem 500

---
+++ 500-2.js
@@ -0,0 +1,35 @@
+const leastWith2ToTheNDivisors = (log2OfDivisors) => {
+	// debugger;
+	let smallestNumber = Infinity;
+	for(const exponents of Tree.iterate([], function*(exponents) {
+		if(
+			exponents.map(e => e + 1).product() >= 2 ** log2OfDivisors
+		) {
+			// console.log(`reached a leaf at [${exponents.join(", ")}]`);
+			return;
+		}
+		let minExponent = 1;
+		let maxExponent = (exponents.length) ? exponents[exponents.length - 1] : 2 ** log2OfDivisors - 1;
+		for(let exponent = minExponent; exponent <= maxExponent; exponent = exponent * 2 + 1) {
+			yield [...exponents, exponent];
+		}
+	}, false, "bfs")) {
+		// console.log(exponents);
+		// debugger;
+		const divisors = exponents.map(e => e + 1).product();
+		const number = Math.defactorize(exponents);
+		if(number < smallestNumber && divisors === 2 ** log2OfDivisors) {
+			// console.log(`new smallest number is ${number}`);
+			smallestNumber = number;
+		}
+	}
+	return smallestNumber;
+};
+testing.addUnit(leastWith2ToTheNDivisors, [
+	[2, 6n],
+	[3, 24n],
+	[4, 120n],
+	[5, 840n],
+	[6, 7560n],
+	[7, 83160n]
+]);
