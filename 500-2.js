1eca1f9d8e16e9ed187d0b5ca807ee75540b8ee9
HappyRyan2 <HappyRyan2@Hotmail.com>
2022-03-02 20:42:55 -0800

    optimized: narrowed down upper / lower bounds for tree search branches

--- 500-2.js
+++ 500-2.js
@@ -4,11 +4,19 @@ const leastWith2ToTheNDivisors = (log2OfDivisors) => {
 		if(
 			exponents.map(e => e + 1).product() >= 2 ** log2OfDivisors
 		) { return; }
-		let minExponent = 1;
-		let maxExponent = (exponents.length) ? exponents[exponents.length - 1] : 2 ** log2OfDivisors - 1;
-		for(let exponent = minExponent; exponent <= maxExponent; exponent = exponent * 2 + 1) {
-			yield [...exponents, exponent];
-		}
+		const lastExponent = exponents[exponents.length - 1];
+		const lastPrime = Sequence.PRIMES.nthTerm(exponents.length - 1);
+		const nextPrime = Sequence.PRIMES.nthTerm(exponents.length);
+		let minExponent = (exponents.length === 0) ? 1 : (lastExponent + 1) / 2 * Math.logBase(nextPrime, lastPrime) - 1;
+		let maxExponent = (exponents.length) ? Math.min(
+			exponents[exponents.length - 1],
+			2 * (lastExponent + 1) * Math.logBase(nextPrime, lastPrime) - 1
+		) : 2 ** log2OfDivisors - 1;
+		for(
+			let exponent = (minExponent <= 1) ? 1 : Math.max(1, 2 ** (Math.floor(Math.log2(minExponent))) - 1);
+			exponent <= Math.floor(maxExponent);
+			exponent = exponent * 2 + 1
+		) { yield [...exponents, exponent]; }
 	}, false, "bfs")) {
 		const divisors = exponents.map(e => e + 1).product();
 		const number = Math.defactorize(exponents);
