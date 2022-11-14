754c94c5a5bda8f7356342327d7fbe9adcfdf3ea
HappyRyan2 <HappyRyan2@Hotmail.com>
2022-11-13 23:29:06 -0800

    optimized totient function
    This now uses prime factorization and the multiplicativity of the totient function instead of the previous naive approach, but it's still not fast enough.

--- 214.js
+++ 214.js
@@ -1,19 +1,32 @@
 const totient = (n) => {
-	let result = 0;
-	for(let i = 1; i <= n; i ++) {
-		if(Math.gcd(i, n) === 1) {
-			result ++;
-		}
+	let exponentOn2 = 0;
+	while(n % 2 === 0) {
+		n /= 2;
+		exponentOn2 ++;
 	}
+	let result = (exponentOn2 === 0) ? 1 : 2 ** (exponentOn2 - 1);
+	for(let i = 3; i * i <= n; i ++) {
+		let exponent = 0;
+		while(n % i === 0) {
+			n /= i;
+			exponent ++;
+		}
+		result *= (exponent === 0) ? 1 : (i ** (exponent - 1)) * (i - 1);
+	}
+	if(n !== 1) { result *= (n - 1); }
 	return result;
 };
 testing.addUnit("totient()", totient, [
 	[5, 4],
+	[7, 6],
+	[8, 4],
+	[9, 6],
+	[10, 4],
 	[14, 6],
 	[18, 6]
 ]);
 
-const solve = (upperBound = 40000000, length = 25) => {
+const solve = (upperBound = 4e7, length = 25) => {
 	let lengths = [0, 1];
 	let result = 0;
 	for(let i = 2; i < upperBound; i ++) {
@@ -24,4 +37,7 @@ const solve = (upperBound = 40000000, length = 25) => {
 	}
 	return result;
 };
-console.log(solve(1000, 4));
+console.time();
+// console.log(solve(1e5, 25));
+console.log(solve());
+console.timeEnd();
