09ed002a99b6b80591d57694245293cea1a7dccc
HappyRyan2 <HappyRyan2@Hotmail.com>
2022-11-16 22:03:29 -0800

    fixed bugs (solving problem 206)

--- 206.js
+++ 206.js
@@ -1,12 +1,12 @@
 const solve = () => {
 	const upperBound = 1389026624n; // approximately sqrt(1929394959697989990)
 	const lowerBound = 1010101000n; // approximately sqrt(1020304050607080900)
-	for(let i = lowerBound; i < upperBound; i += 100n) {
+	for(let i = lowerBound; i < upperBound; i += 10n) {
 		const square = i ** 2n;
 		const digits = `${square}`;
 		if(digits.length === 19 && (
-			digits[0] === "1" && digits[2] === "2" && digits[4] === "3" && digits[6] === "4" &&
-			digits[8] === "5" && digits[10] === "6" && digits[12] === "7" && digits[14] === "9"
+			digits[0] === "1" && digits[2] === "2" && digits[4] === "3" && digits[6] === "4" && digits[8] === "5" &&
+			digits[10] === "6" && digits[12] === "7" && digits[14] === "8" && digits[16] === "9" && digits[18] === "0"
 		)) {
 			console.log(`the answer is ${i}`);
 			return i;
