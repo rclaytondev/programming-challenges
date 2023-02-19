32ca5486b2d9987ca5554ef6e1b971dbbac9241f
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-02-19 11:24:40 -0800

    Update 823.js

--- 823.js
+++ 823.js
@@ -1,9 +1,10 @@
 const solve = (upperBound = 1000, iterations = 10 ** 16) => {
 	const initialNumbers = new Array(upperBound - 1).fill().map((v, i) => i + 2); // 2 to upperBound, inclusive
+	const previousIterations = [[...initialNumbers]];
 	let numbers = [...initialNumbers];
 	for(let i = 0; i < iterations; i ++) {
 		let factorProduct = 1;
-		const nextNumbers = [];
+		let nextNumbers = [];
 		for(const num of numbers) {
 			let smallestFactor;
 			for(const prime of Sequence.PRIMES) {
@@ -16,13 +17,20 @@ const solve = (upperBound = 1000, iterations = 10 ** 16) => {
 			if(num !== smallestFactor) { nextNumbers.push(num / smallestFactor); }
 		}
 		nextNumbers.push(factorProduct);
+		nextNumbers = nextNumbers.sort((a, b) => a - b);
 		numbers = nextNumbers;
-		console.log(numbers);
+		const periodicityIndex = previousIterations.findIndex(iteration => iteration.equals(nextNumbers));
+		if(periodicityIndex !== -1) {
+			// periodicity detected!
+			const periodLength = i - periodicityIndex;
+			debugger;
+		}
+		previousIterations.push(nextNumbers);
 	}
 	return numbers.sum();
 };
 
 testing.addUnit(solve, [
-	[5, 3, 21],
+	// [5, 3, 21],
 	[10, 100, 257]
 ]);
