e32c5e410ccf4c5140b1b9ab0516c6bb57e9ae40
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-07-30 12:50:29 -0700

    changed from PrettierJS to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        // "es2021": true
-    },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+	"env": {
+		"node": true,
+		"es2021": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"parserOptions": {
+		"ecmaVersion": 12,
+		"sourceType": "module",
+		"parser": "@typescript-eslint/parser"
+	},
+	"rules": {
+		"quotes": ["error", "double"],
+		"semi": ["error", "always"],
+		"indent": ["error", "tab"],
+		"no-multi-spaces": ["error"]
+	},
+	"plugins": [
+		"@typescript-eslint"
+	]
 };
