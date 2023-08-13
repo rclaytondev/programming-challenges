1baa61d37c2fff59a462082fcaec8d7c1eb45b4a
HappyRyan2 <HappyRyan2@Hotmail.com>
2023-08-13 12:08:42 -0700

    changed comma-dangle to always-multiline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
 	"env": {
 		"node": true,
-		"es2021": true
+		"es2021": true,
 	},
 	"extends": [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended"
+		"plugin:@typescript-eslint/recommended",
 	],
 	"parserOptions": {
 		"ecmaVersion": 12,
 		"sourceType": "module",
-		"parser": "@typescript-eslint/parser"
+		"parser": "@typescript-eslint/parser",
 	},
 	"rules": {
 		"quotes": ["error", "double"],
 		"semi": ["error", "always"],
 		"indent": ["error", "tab"],
-		"no-multi-spaces": ["error"]
+		"no-multi-spaces": ["error"],
+		"comma-dangle": ["error", "always-multiline"],
 	},
 	"plugins": [
-		"@typescript-eslint"
-	]
+		"@typescript-eslint",
+	],
 };
