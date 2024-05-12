c0695756641372c0414152c1d7fc237619cafdf6
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-05-11 18:33:51 -0700

    replaced 0 and 1 with "even" and "odd"

--- project-euler-ts/502-counting-castles/counting-castles.ts
+++ project-euler-ts/502-counting-castles/counting-castles.ts
@@ -6,9 +6,9 @@ import { generalizedModulo } from "../utils-ts/Math";
 class GraphNode {
 	x: number;
 	y: number;
-	parity: 0 | 1;
+	parity: "even" | "odd";
 
-	constructor(x: number, y: number, parity: 0 | 1) {
+	constructor(x: number, y: number, parity: "even" | "odd") {
 		this.x = x;
 		this.y = y;
 		this.parity = parity;
@@ -17,16 +17,16 @@ class GraphNode {
 	predecessors(height: number) {
 		if(this.x === 0) { return []; }
 		if(this.x === 1) {
-			return (this.y % 2 === this.parity) ? [new GraphNode(0, 0, 0)] : [];
+			return ((this.y % 2 === 0) === (this.parity === "even")) ? [new GraphNode(0, 0, "even")] : [];
 		}
 		const predecessors = [];
 		for(let y = 0; y <= height; y ++) {
 			if(y <= this.y) {
-				predecessors.push(new GraphNode(this.x - 1, y, (1 - ((this.y - y) % 2)) as 0 | 1));
+				predecessors.push(new GraphNode(this.x - 1, y, (1 - ((this.y - y) % 2)) === 0 ? "even" : "odd"));
 			}
 			else if(y > this.y) {
-				predecessors.push(new GraphNode(this.x - 1, y, 0));
-				predecessors.push(new GraphNode(this.x - 1, y, 1));
+				predecessors.push(new GraphNode(this.x - 1, y, "even"));
+				predecessors.push(new GraphNode(this.x - 1, y, "odd"));
 			}
 		}
 		return predecessors;
@@ -37,18 +37,18 @@ class GraphNode {
 }
 
 const initializeGraph = (width: number, height: number): [GraphNode[], Map<string, number>] => {
-	const nodes: GraphNode[] = [new GraphNode(0, 0, 0)];
-	const reverseNodeMap: Map<string, number> = new Map([["0,0,0", 0]]);
+	const nodes: GraphNode[] = [new GraphNode(0, 0, "even")];
+	const reverseNodeMap: Map<string, number> = new Map([["0,0,even", 0]]);
 	for(let x = 1; x <= width; x ++) {
 		for(let y = 0; y <= height; y ++) {
-			nodes.push(new GraphNode(x, y, 0));
-			reverseNodeMap.set(`${x},${y},0`, nodes.length - 1);
-			nodes.push(new GraphNode(x, y, 1));
-			reverseNodeMap.set(`${x},${y},1`, nodes.length - 1);
+			nodes.push(new GraphNode(x, y, "even"));
+			reverseNodeMap.set(`${x},${y},even`, nodes.length - 1);
+			nodes.push(new GraphNode(x, y, "odd"));
+			reverseNodeMap.set(`${x},${y},odd`, nodes.length - 1);
 		}
 	}
-	nodes.push(new GraphNode(width + 1, 0, 1));
-	reverseNodeMap.set(`${width + 1},0,1`, nodes.length - 1);
+	nodes.push(new GraphNode(width + 1, 0, "odd"));
+	reverseNodeMap.set(`${width + 1},0,odd`, nodes.length - 1);
 	return [nodes, reverseNodeMap];
 };
 const getAdjacencyMatrix = (nodes: GraphNode[], reverseNodeMap: Map<string, number>, height: number, modulo: number) : Matrix<number> => {
@@ -70,8 +70,8 @@ const nonFullHeightOddCastlesWithoutBase = (width: number, height: number, modul
 	const adjacencyMatrix = getAdjacencyMatrix(nodes, reverseNodeMap, height, modulo);
 	const sumOfPowers = Matrix.identity(adjacencyMatrix.field, adjacencyMatrix.width).subtract(adjacencyMatrix).inverse();
 	return sumOfPowers!.get(
-		reverseNodeMap.get("0,0,0")!,
-		reverseNodeMap.get(`${width + 1},0,1`)!,
+		reverseNodeMap.get("0,0,even")!,
+		reverseNodeMap.get(`${width + 1},0,odd`)!,
 	);
 };
 const castles = (width: number, height: number, modulo: number) => {
@@ -82,30 +82,30 @@ const castles = (width: number, height: number, modulo: number) => {
 
 describe("GraphNode.predecessors", () => {
 	it("returns an empty list for an x=1 node where the parity does not match the parity of the y-value", () => {
-		const node = new GraphNode(1, 3, 0);
+		const node = new GraphNode(1, 3, "even");
 		assert.sameDeepOrderedMembers(node.predecessors(3), []);
 	});
-	it("returns a list containing the (0, 0, 0) node for an x=1 node when the parity matches the parity of the y-value", () => {
-		const node = new GraphNode(1, 3, 1);
-		assert.sameDeepOrderedMembers(node.predecessors(3), [new GraphNode(0, 0, 0)]);
+	it("returns a list containing the (0, 0, even) node for an x=1 node when the parity matches the parity of the y-value", () => {
+		const node = new GraphNode(1, 3, "odd");
+		assert.sameDeepOrderedMembers(node.predecessors(3), [new GraphNode(0, 0, "even")]);
 	});
 	it("returns the empty list for an x=0 node", () => {
-		const node = new GraphNode(0, 2, 0);
+		const node = new GraphNode(0, 2, "even");
 		assert.sameDeepOrderedMembers(node.predecessors(3), []);
 	});
 	it("returns all the predecessors for a node with x>1", () => {
-		const node = new GraphNode(2, 2, 1);
+		const node = new GraphNode(2, 2, "odd");
 		assert.sameDeepMembers(node.predecessors(5), [
-			new GraphNode(1, 0, 1),
-			new GraphNode(1, 1, 0),
-			new GraphNode(1, 2, 1),
+			new GraphNode(1, 0, "odd"),
+			new GraphNode(1, 1, "even"),
+			new GraphNode(1, 2, "odd"),
 
-			new GraphNode(1, 3, 0),
-			new GraphNode(1, 3, 1),
-			new GraphNode(1, 4, 0),
-			new GraphNode(1, 4, 1),
-			new GraphNode(1, 5, 0),
-			new GraphNode(1, 5, 1),
+			new GraphNode(1, 3, "even"),
+			new GraphNode(1, 3, "odd"),
+			new GraphNode(1, 4, "even"),
+			new GraphNode(1, 4, "odd"),
+			new GraphNode(1, 5, "even"),
+			new GraphNode(1, 5, "odd"),
 		]);
 	});
 });
