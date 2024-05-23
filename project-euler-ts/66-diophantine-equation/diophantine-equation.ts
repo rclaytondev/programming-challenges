772fa296db988540cd5bdb1226e42305e696826b
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-05-22 18:12:38 -0700

    added inefficient solution to problem 66

---
+++ project-euler-ts/66-diophantine-equation/diophantine-equation.ts
@@ -0,0 +1,24 @@
+const minSolution = (D: number) => {
+	for(let x = 2; x < Infinity; x ++) {
+		if(Math.sqrt((x ** 2 - 1) / D) % 1 === 0) {
+			return x;
+		}
+	}
+	throw new Error("Reached end of infinite loop.");
+};
+
+const solve = () => {
+	let highestD = 0;
+	let highestMinSolution = -Infinity;
+	for(let D = 1; D < 1000; D ++) {
+		if(Math.sqrt(D) % 1 === 0) { continue; }
+		const solution =  minSolution(D);
+		console.log(`For D=${D}, the minimal solution has x=${solution}`);
+		if(solution > highestMinSolution) {
+			highestMinSolution = solution;
+			highestD = D;
+		}
+	}
+	return highestD;
+};
+console.log(solve());
