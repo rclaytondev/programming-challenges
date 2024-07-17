746479e38f3768f7ac062628da66e5029b8263f8
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-07-16 18:47:53 -0700

    refactored

--- project-euler-haskell/1-multiples-of-3-or-5/multiples-of-3-or-5.hs
+++ project-euler-haskell/1-multiples-of-3-or-5/multiples-of-3-or-5.hs
@@ -1,5 +1,5 @@
 main = do
     print(solve 999)
 
-solve 0 = 0
-solve upperBound = if mod upperBound 3 == 0 || mod upperBound 5 == 0 then upperBound + solve(upperBound - 1) else solve(upperBound - 1)
+divides a b = mod b a == 0
+solve upperBound = sum(filter (\n -> divides 3 n || divides 5 n) [1 .. upperBound])
