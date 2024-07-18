85a632f75a960a902b46661e735d7d5b5a1a8989
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-07-17 22:13:49 -0700

    optimized (not enough)
    This optimization only checks numbers above the square root, but it's not fast enough.

--- project-euler-haskell/3-largest-prime-factor/largest-prime-factor.hs
+++ project-euler-haskell/3-largest-prime-factor/largest-prime-factor.hs
@@ -1,11 +1,14 @@
 import Data.List (find)
 divides a b = b `mod` a == 0
 
-divisors num = filter (`divides` num) [1 .. num]
+divisorsAboveSqrt num = filter (`divides` num) [floorSqrt num .. num]
+
+floorSqrt num = floor(sqrt(fromIntegral num))
 
 isPrime num = not(any (`divides` num) [2 .. num - 1])
 
-largestPrimeFactor num = find isPrime (reverse(divisors num))
+largestPrimeFactor num = find isPrime (reverse(divisorsAboveSqrt num))
 
 main = do
+    -- print(divisorsAboveSqrt 123)
     print(largestPrimeFactor 600851475143)
