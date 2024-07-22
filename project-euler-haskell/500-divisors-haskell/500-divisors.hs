65d7e10a15c98d4eba87a33b6e5aa7e8fa61111a
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-07-21 23:12:52 -0700

    added function getLogMultiplier

--- project-euler-haskell/500-divisors-haskell/500-divisors.hs
+++ project-euler-haskell/500-divisors-haskell/500-divisors.hs
@@ -7,8 +7,11 @@ isPrime num = num >= 2 && not(any (`divides` num) [2 .. num - 1])
 
 nextPrime prime = fromJust(find isPrime [prime + 1 ..])
 
-primes = iterate nextPrime 2
+primes = iterate nextPrime (2 :: Integer)
+
+getLogMultiplier expFactorization index = (2 ^ (expFactorization !! index)) * log(fromInteger(primes !! index))
 
 main = do
     print(nextPrime 13 == 17)
     print(take 5 primes == [2, 3, 5, 7, 11])
+    print(getLogMultiplier [1, 1] 0 == log 4)
