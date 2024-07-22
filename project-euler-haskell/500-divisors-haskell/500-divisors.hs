9023de347d534a5bc076113a1c6dd0f4c3876226
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-07-21 23:15:18 -0700

    added function minIndex

--- project-euler-haskell/500-divisors-haskell/500-divisors.hs
+++ project-euler-haskell/500-divisors-haskell/500-divisors.hs
@@ -11,7 +11,12 @@ primes = iterate nextPrime (2 :: Integer)
 
 getLogMultiplier expFactorization index = (2 ^ (expFactorization !! index)) * log(fromInteger(primes !! index))
 
+minIndex [x] = 0
+minIndex (x:xs) = if x < xs !! min then 0 else min + 1
+    where min = minIndex xs
+
 main = do
     print(nextPrime 13 == 17)
     print(take 5 primes == [2, 3, 5, 7, 11])
     print(getLogMultiplier [1, 1] 0 == log 4)
+    print(minIndex [10, 3, 1, 5] == 2)
