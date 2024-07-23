937ea0cba7c54aa77c11d142405b5885b36789d1
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-07-22 22:15:52 -0700

    optimized

--- project-euler-haskell/500-divisors-haskell/500-divisors.hs
+++ project-euler-haskell/500-divisors-haskell/500-divisors.hs
@@ -1,5 +1,6 @@
 import Data.List (find)
 import Data.Maybe (fromJust)
+import qualified Data.Set as Set
 
 divides a b = b `mod` a == 0
 
@@ -15,17 +16,32 @@ minIndex [x] = 0
 minIndex (x:xs) = if x < xs !! min then 0 else min + 1
     where min = minIndex xs
 
+-- argmin f [x] = x
+-- argmin f (x:xs) = 
+
+minEntry f [x] = (x, f x)
+minEntry f (x:xs) = if value < minOutput then (x, value) else (minValue, minOutput)
+    where
+        value = f x
+        (minValue, minOutput) = minEntry f xs
+
+argmin f arr = fst(minEntry f arr)
+
 replace (x:xs) 0 value = value : xs
 replace (x:xs) index value = x : replace xs (index - 1) value
 
 endWith array value = if last array == value then array else array ++ [value]
 
-nextFactorization expFactorization = endWith (replace expFactorization index newExponent) 0
+nextFactorization (expFactorization, possibleIndices) = (newFactorization, newPossibleIndices)
     where
-        index = minIndex (map (getLogMultiplier expFactorization) [0 .. length expFactorization - 1])
+        index = argmin (getLogMultiplier expFactorization) (Set.toAscList possibleIndices)
         newExponent = (expFactorization !! index) + 1
+        newFactorization = endWith (replace expFactorization index newExponent) 0
+        newPossibleIndices = if index /= 0 && newFactorization !! index == newFactorization !! (index - 1)
+            then Set.insert (index + 1) (Set.delete index possibleIndices)
+            else Set.insert (index + 1) possibleIndices
 
-solve log2OfDivisors = iterate nextFactorization [0] !! log2OfDivisors
+solve log2OfDivisors = iterate nextFactorization ([0], Set.fromList [0]) !! log2OfDivisors
 
 entries [] = []
 entries (x:xs) = (0, x) : map (\(i, v) -> (i+1, v)) (entries xs)
@@ -44,10 +60,10 @@ main = do
     print(getLogMultiplier [1, 0] 0 == log 4)
     print(minIndex [10, 3, 1, 5] == 2)
     print(replace [5, 4, 3, 2, 1] 2 100 == [5, 4, 100, 2, 1])
-    print(nextFactorization [1, 0] == [1, 1, 0])
-    print(nextFactorization [1, 1, 0] == [2, 1, 0])
-    print(solve 4 == [2, 1, 1, 0])
-    print(toNumber(solve 4) 150 == 120)
-    print(toNumber(solve 4) 97 == 23)
+    print(nextFactorization ([1, 0], Set.fromList [0, 1]) == ([1, 1, 0], Set.fromList [0, 2]))
+    print(nextFactorization ([1, 1, 0], Set.fromList [0, 2]) == ([2, 1, 0], Set.fromList [0, 1, 2]))
+    print(fst (solve 4) == [2, 1, 1, 0])
+    print(toNumber(fst (solve 4)) 150 == 120)
+    print(toNumber(fst (solve 4)) 97 == 23)
 
-    -- print(solve 1000)
+    print(solve 1000)
