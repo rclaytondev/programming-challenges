597f159c99d421a150f136d6f2e566ca5a6beb12
HappyRyan2 <HappyRyan2@Hotmail.com>
2024-08-03 00:11:11 -0700

    fixed the rest of the algorithm
    Right now it's too slow. I let it run for 13 minutes and it wasn't done, and I expect it to take much longer than that (3 hours if it was linear time, but of course it's much worse than linear).

--- project-euler-haskell/893-matchsticks/matchsticks-2.hs
+++ project-euler-haskell/893-matchsticks/matchsticks-2.hs
@@ -2,21 +2,23 @@ digitMatchsticks = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 
 a `divides` b = b `mod` a == 0
 
-divisors n = filter (`divides` n) [1 .. n-1]
+divisors n = filter (`divides` n) [2 .. n-1]
 
 digits n
     | n < 10 = [n]
     | otherwise = digits (n `div` 10) ++ [n `mod` 10]
 
+matchsticksForDigits n = sum(map (digitMatchsticks !!) (digits n))
+
 nextProductMatchsticks values = values ++ [nextValue]
     where
         n = length values
         representations = map (\d -> values !! d + values !! (n `div` d) + 2) (divisors n)
-        nextValue = minimum(if n < 10 then representations ++ [digitMatchsticks !! n] else representations)
+        nextValue = minimum(matchsticksForDigits n : representations)
 
 productMatchsticksList = iterate nextProductMatchsticks []
 
-productMatchsticks = map last productMatchsticksList
+productMatchsticks = map last (tail productMatchsticksList)
 
 recursivePartitions num size lowerBound
     | size == 0 = [[] | num == 0]
@@ -30,11 +32,17 @@ partitionsWithMaxSize num maxSize = concatMap (partitions num) [1 .. maxSize]
 
 matchsticks n = minimum (map matchsticksFor (partitionsWithMaxSize n maxSize))
     where
-        maxSize = n `div` 4
+        maxSize = max 1 (matchsticksForDigits n `div` 4)
         matchsticksFor partition = 2 * (length partition - 1) + sum(map (productMatchsticks !!) partition)
 
+matchsticksSum n = sum (map matchsticks [1 .. n])
+
 main = do
-    -- print (matchsticks 28)
     print(partitions 5 1 == [[5]])
     print(partitions 5 2 == [[1, 4], [2, 3]])
     print(partitions 5 3 == [[1, 1, 3], [1, 2, 2]])
+    print(partitions 1 1 == [[1]])
+
+    print (matchsticks 28 == 9)
+    print (matchsticksSum 100 == 916)
+    print (matchsticksSum 1000000)
